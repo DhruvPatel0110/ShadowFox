e1eac2e441c53b3a4e654e0301c40f57c65a0622
Dhruv Patel <pateldpp0110@gmail.com>
2026-05-15 02:31:19 +0000

    Implimented variables

---
+++ Task_level_1/Variables.py
@@ -0,0 +1,12 @@
+pi=22/7
+print(type(pi))
+#output : <class 'float'>
+# for=4
+# print(for)
+#output : SyntaxError: invalid syntax
+Principal_amt = 1000
+Rate_of_interest = 10
+Time = 3 
+Simple_interest = (Principal_amt * Rate_of_interest * Time) / 100
+print(f"Simple interest : {Simple_interest}")
+#output: Simple interest : 300.0
